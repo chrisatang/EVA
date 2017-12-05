9653f1ef0639d558f6cff0c5f3e9e60653f98d96
Subin Samuel <subinsamuel@rpi-wl-1046.dynamic.rpi.edu>
2017-12-05 15:50:53 -0500

    Made the demo on website

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res){
-	res.render('index');
+	//res.render('index');
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log("a user connected");
+
+  socket.on('chat message', function(msg){
+  	io.emit('chat message', msg);
+
+  	if(msg.toLowerCase().indexOf("where is") != -1){
+  		io.emit('where', msg);
+  	}
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
